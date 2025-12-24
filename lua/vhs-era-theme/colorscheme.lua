7824484a448ccc9e3bcb46d6e5454b8885b863d2
Marco Kellershoff <1384938+gorillamoe@users.noreply.github.com>
2025-12-24 12:45:06 +0100

    fix(lib): hot-reload (#2)

--- lua/vhs-era-theme/colorscheme.lua
+++ lua/vhs-era-theme/colorscheme.lua
@@ -1,17 +1,22 @@
 local cache = require("vhs-era-theme.lib.cache")
-local colors = {}
 
-if cache.exists() then
-  colors = cache.read()
-else
-  colors = require("vhs-era-theme.palette.colors")
-  cache.write(colors)
+local function load_colorscheme()
+  local colors = {}
+
+  if cache.exists() then
+    colors = cache.read()
+  else
+    colors = require("vhs-era-theme.palette.colors")
+    cache.write(colors)
+  end
+
+  vim.cmd("highlight clear")
+  vim.cmd("set t_Co=256")
+
+  for group, attrs in pairs(colors) do
+    vim.api.nvim_set_hl(0, group, attrs)
+  end
 end
 
-vim.cmd("highlight clear")
-vim.cmd("set t_Co=256")
-
-for group, attrs in pairs(colors) do
-  vim.api.nvim_set_hl(0, group, attrs)
-end
+return load_colorscheme
 
