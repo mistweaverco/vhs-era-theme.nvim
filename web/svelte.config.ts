c1dc20eb539ebf8a21c29c39a02fdc442b8a0f74
Marco Kellershoff <marco@mistweaverco.com>
2026-01-02 00:35:22 +0100

    fix(web): build

--- web/svelte.config.ts
+++ web/svelte.config.ts
@@ -2,11 +2,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import type { Config } from '@sveltejs/kit';
 import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex';
-import mdsvexShiki from '@mistweaverco/mdsvex-shiki';
 
 const config: Config = {
   kit: {
+    prerender: {
+      handleMissingId: 'ignore'
+    },
     adapter: adapter({
       precompress: true
     })
@@ -16,11 +17,6 @@ const config: Config = {
   preprocess: [
     vitePreprocess({
       script: true
-    }),
-    mdsvex({
-      highlight: {
-        highlighter: await mdsvexShiki({})
-      }
     })
   ]
 };
